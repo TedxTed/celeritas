305d4f1c5fd8eab6a04c937115de3565a79969ea
Ted <wodf410@gmail.com>
2024-04-25 00:39:34 +0800

    Add logging and versioning to Celeritas struct

--- celeritas.go
+++ celeritas.go
@@ -2,15 +2,23 @@ package celeritas
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
 type Celeritas struct {
-	AppName string
-	Debug   bool
-	Version string
+	AppName  string
+	Debug    bool
+	Version  string
+	ErrorLog *log.Logger
+	InfoLog  *log.Logger
+	RootPath string
 }
+
+var version = "1.0.0"
 
 func (c *Celeritas) New(rootPath string) error {
 	pathConfig := initPaths{
@@ -33,6 +41,13 @@ func (c *Celeritas) New(rootPath string) error {
 	if err != nil {
 		return err
 	}
+
+	// create loggers
+	infoLog, errorLog := c.startLoggers()
+	c.InfoLog = infoLog
+	c.ErrorLog = errorLog
+	c.Debug, _ = strconv.ParseBool(os.Getenv("DEBUG"))
+	c.Version = version
 
 	return nil
 }
@@ -58,4 +73,14 @@ func (c *Celeritas) checkDotEnv(path string) error {
 
 	return nil
 
+}
+
+func (c *Celeritas) startLoggers() (*log.Logger, *log.Logger) {
+	var infoLog *log.Logger
+	var errorLog *log.Logger
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	return infoLog, errorLog
 }
